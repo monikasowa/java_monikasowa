9463695dea775f992ac009302a8344d7bc286efe
monikasowa <42975630+monikasowa@users.noreply.github.com>
2018-09-13 20:44:48 +0200

    Parametryzacja dla kontaktu

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/KontaktCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/KontaktCreationTests.java
@@ -18,21 +18,21 @@ public class KontaktCreationTests {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox ESR/firefox.exe"));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
-        Login();
+        Login("admin", "secret");
 
     }
 
-    private void Login() {
+    private void Login(String username, String password) {
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).sendKeys("\\undefined");
         wd.findElement(By.cssSelector("html")).click();
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
