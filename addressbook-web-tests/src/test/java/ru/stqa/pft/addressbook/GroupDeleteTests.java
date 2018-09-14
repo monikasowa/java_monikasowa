bfcb79973617cabd6892a6aa42e0964c3e214aaf
monikasowa <42975630+monikasowa@users.noreply.github.com>
2018-09-14 20:41:09 +0200

    Metody pomocnicze zainicjowane zTestBase dla usuniętej grupy

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeleteTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeleteTests.java
@@ -1,10 +1,6 @@
 package ru.stqa.pft.addressbook;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
-
 import org.openqa.selenium.*;
 
 
@@ -12,12 +8,13 @@ public class GroupDeleteTests extends TestBase {
 
     @Test
     public void testGroupDelete() {
-      
+
         wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.name("selected[]")).click();
-        wd.findElement(By.name("delete")).click();
-        wd.findElement(By.linkText("group page")).click();
+        selectGroup();
+        deleteSelectedGroups();
+        returntoGroupPage();
     }
+
 }
     
 
