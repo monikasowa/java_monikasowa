ab72d377f8be3a89588b82fcfa1d567254d94f56
monikasowa <42975630+monikasowa@users.noreply.github.com>
2018-09-14 18:58:05 +0200

    Revert "Revert "GroupKontakt dla kontaktu""
    This reverts commit 2f6aed40dd8506789d3ec01f7b8f2ec7737ffb79.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupKontakt.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupKontakt {
+    private final String firstname;
+
+    public GroupKontakt(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+}
