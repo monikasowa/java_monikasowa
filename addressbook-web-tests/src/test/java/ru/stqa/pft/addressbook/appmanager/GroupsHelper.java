55b792d2460b923dbe5c68a12015d7178322515a
monikasowa <42975630+monikasowa@users.noreply.github.com>
2018-09-15 22:00:42 +0200

    Revert "Revert "GroupsHelper dla grupy""
    This reverts commit f75cc5a137b03bfe45e0f6a6c8166288ee39025f.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupsHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupsHelper.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupsHelper {
-    private FirefoxDriver wd;
+    public FirefoxDriver wd;
 
 
     public GroupsHelper(FirefoxDriver wd) {
