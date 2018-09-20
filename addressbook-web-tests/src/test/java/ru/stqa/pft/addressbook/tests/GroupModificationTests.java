ecf20e2ed215697b15a9322313b930e026585083
monikasowa <42975630+monikasowa@users.noreply.github.com>
2018-09-20 22:20:53 +0200

    Warunki wstępne modyfikacja

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,21 +1,32 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-public class GroupModificationTests extends TestBase{
+
+public class GroupModificationTests extends TestBase {
 
     @Test
+
     public void testGroupMOdification() {
 
+
         app.getGroupsHelper().wd.findElement(By.linkText("groups")).click();
+        if(!app.getGroupsHelper().isThereAGroup()){
+            app.getGroupsHelper().createGroup(new GroupData("test4",null, null));
+        }
         app.getGroupsHelper().selectGroup();
         app.getGroupsHelper().initGroupModification();
-        app.getGroupsHelper().fillGroupForm(new GroupData("test7","test8", "test9"));
+        app.getGroupsHelper().fillGroupForm(new GroupData("test7", "test8", "test9"));
         app.getGroupsHelper().submitGroupModification();
         app.getGroupsHelper().returntoGroupPage();
-
     }
+
 }
+
+
+
+
 
