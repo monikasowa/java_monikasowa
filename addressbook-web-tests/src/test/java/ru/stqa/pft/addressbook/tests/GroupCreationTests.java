dfc06850b9036c97bcf59f0892e1ded36a825e50
monikasowa <42975630+monikasowa@users.noreply.github.com>
2018-09-30 23:43:58 +0200

    Porównanie list - tworzona grupa

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,27 +5,36 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
-        //List<GroupData> before = app.getGroupsHelper().getGroupList();
+        List<GroupData> before = app.getGroupsHelper().getGroupList();
+        GroupData group = new GroupData("test4", null, null);
         app.getGroupsHelper().initGroupCreation();
         app.getGroupsHelper().fillGroupForm(new GroupData("test4", null, null));
         app.getGroupsHelper().submitGroupCreation();
         app.getGroupsHelper().returntoGroupPage();
 
-        //List<GroupData> after = app.getGroupsHelper().getGroupList();
+        List<GroupData> after = app.getGroupsHelper().getGroupList();
 
-        //Assert.assertEquals(after.size(), before.size()+1);
+        Assert.assertEquals(after.size(), before.size() + 1);
 
-        //before.add(new GroupData("test4", null, null));
-        //Assert.assertEquals(before, after);
-        //  }
 
+        int max = 0;
+        for (GroupData g : after) {
+            if (g.getId() > max) {
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
+
 
 
