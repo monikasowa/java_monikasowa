32ad3a918ae07de3e2ced232c4c64a1c7e27c976
monikasowa <42975630+monikasowa@users.noreply.github.com>
2018-10-05 09:57:32 +0200

    Zadanie 10 wszystko ok - grupy

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -17,22 +18,14 @@ public class GroupCreationTests extends TestBase {
         app.getGroupsHelper().fillGroupForm(new GroupData("test4", null, null));
         app.getGroupsHelper().submitGroupCreation();
         app.getGroupsHelper().returntoGroupPage();
-
         List<GroupData> after = app.getGroupsHelper().getGroupList();
-
         Assert.assertEquals(after.size(), before.size() + 1);
 
-
-
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData > byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
-
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -29,19 +29,23 @@ public class GroupModificationTests extends TestBase {
         app.getGroupsHelper().fillGroupForm(group);
         app.getGroupsHelper().submitGroupModification();
         app.getGroupsHelper().returntoGroupPage();
-        
+
         List<GroupData> after = app.getGroupsHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
 
         before.remove(before.size()-1);
         before.add(group);
+        Comparator<? super GroupData > byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+        before.sort(byId);
+        after.sort(byId);
         Assert.assertEquals(before, after);
         //before.set(before.size() - 1,new GroupData("test7", "test8", "test9"));
-
-       // Comparator<? super GroupData > byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
-       // before.sort(byId);
-        // after.sort(byId);
     }
 
 }
+
+
+
+
